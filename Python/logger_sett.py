802aba1ecd53dbde993cb8a9ef050e34a0ab7703
anton <offackoch@gmail.com>
2019-06-18 11:59:29 +0300

    added logger settings

---
+++ Python/logger_sett.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import logging
+
+
+def get_file_name():
+    file_name = datetime.now().strftime("%d.%m.%Y%Hh%Mm")
+    return 'logs/{}.log'.format(file_name)
+
+
+def get_format_file():
+    f = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    df = '[%Y-%m-%d - %H:%M:%S]'
+    return logging.Formatter(f, datefmt=df)
+
+
+def get_format_console():
+    f = '[%(levelname)s] %(message)s'
+    return logging.Formatter(f)
+
+def logger_set_up():
+    logger = logging.getLogger('first_logger')
+    log_file = logging.FileHandler(get_file_name())
+    log_file.setFormatter(get_format_file())
+
+    log_console = logging.StreamHandler()
+    log_console.setFormatter(get_format_console)
+
+    logger.setLevel(logging.DEBUG)
+    log_file.setLevel(logging.DEBUG)
+    log_console.setLevel(logging.DEBUG)
+
+    logger.addHandler(log_file)
+    logger.addHandler(log_console)
